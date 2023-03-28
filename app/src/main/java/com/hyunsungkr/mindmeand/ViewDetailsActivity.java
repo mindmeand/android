4fe7896d3b05b5176500a1a0339afd608bb276f7
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-03-28 11:56:22 +0900

    UI

---
+++ app/src/main/java/com/hyunsungkr/mindmeand/ViewDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.hyunsungkr.mindmeand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_details);
+    }
+}
